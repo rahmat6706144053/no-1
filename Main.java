9385b55c96d2bba15c76fc2ad9493422dd44805c
rahmat kurnianto <rahmatstroger@gmail.com>
2016-03-29 16:06:21 +0700

    fobi_baru

--- Main.java
+++ Main.java
@@ -1,37 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal;
 
-package pkg1;
-
-
+/**
+ *
+ * @author Praktikum
+ */
 import java.util.Scanner;
-
-public class Main {
-public int angka;
-public int x;
-
-
-	public static void main(String[] args)
-	{
+public class Jurnal
+{
+	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
-		System.out.print("Masukkanpa angka = ");
+		System.out.print("Input: ");
 		int n = s.nextInt();
-		for(int i=1;i<=n;i++)
-			System.out.print(fibonaci(i) + "  ");
-		
+                int hasil=0;
+		for(int i=1;i<=n;i++){
+                    hasil = hasil + fibo(i);
+                }
+		System.out.println(hasil);
 	}
 
-	private static int fibonaci(int z)
+	private static int fibo(int n)
 	{
-            int f;
-		if(z==1 || z==2){
-			f=1;
+		int f;
+		if(n==1 || n==2){
+                    f=1;
                 }
                 else{
-			f = fibonaci(z-1) + fibonaci(z-2);
+                    f = fibo(n-1) + fibo(n-2);
                 }
 		return f;
 	}
-    
 }
-
-
-
