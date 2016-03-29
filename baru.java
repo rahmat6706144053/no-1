e972dae97161176aeda4b2dc6dbf58bddeb3d148
rahmat kurnianto <rahmatstroger@gmail.com>
2016-03-29 16:19:04 +0700

    updete 1e

---
+++ baru.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+
+/**
+yang baru
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+ */
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input: ");
+		int n = s.nextInt();
+                int hasil=0;
+		for(int i=1;i<=n;i++){
+                    hasil = hasil + fibo(i);
+                }
+		System.out.println(hasil);
+	}
+
+	private static int fibo(int n)
+	{
+		int f;
+		if(n==1 || n==2){
+                    f=1;
+                }
+                else{
+                    f = fibo(n-1) + fibo(n-2);
+                }
+		return f;
+	}
+}
